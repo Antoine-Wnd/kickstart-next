e55c1ba847855e04e327b28bf009df9fce3c3f58
Antoine-Wnd <yordray59@gmail.com>
2024-02-22 15:35:37 +0100

    add Shadcn-UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,45 @@
 import factory from "../../ethereum/factory";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { PlusCircledIcon } from "@radix-ui/react-icons";
+
 import "./globals.css";
-
-interface CampaignIndexProps {
-  campaigns: string[];
-}
-
 export default async function CampaignIndex() {
   const campaigns = await FetchCampagn();
 
   console.log(campaigns);
+
   return (
     <div>
-      <h1 className=""> test</h1>
-      <ul>
-        {campaigns.map((campaign, index) => (
-          <li key={index}>{campaign}</li>
-        ))}
-      </ul>
+      <Card>
+        <CardHeader>
+          <CardTitle>Campagnes ouvertes</CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div>
+            {campaigns.map((campaign, index) => (
+              <div
+                key={index}
+                className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+              >
+                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-[#003049]" />
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">{campaign}</p>
+                  <p className="text-sm text-muted-foreground">
+                    <a href="#" className="text-[#669bbc] font-bold">
+                      Voir la campagne
+                    </a>
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </CardContent>
+      </Card>
+
+      <Button>
+        <PlusCircledIcon className="mr-2 h-4 w-4" /> Login with Email
+      </Button>
     </div>
   );
 }
