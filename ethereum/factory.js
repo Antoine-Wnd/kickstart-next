cce59b19f5772a0b9eca89c3047dbed1571aec28
Antoine-Wnd <yordray59@gmail.com>
2024-02-28 16:46:45 +0100

    reploy contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   CampaignFactory.abi,
-  "0x9A1B5B35D0b0e3099A84F030225e61aD9BD4c1f3"
+  "0x10E05098203bDf7b7E0803C78003C58e2F5Af97a"
 );
 
 export default instance;
